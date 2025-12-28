9bb08581b8323b2d3e4abc6f1693a68e7778072b
Aiman Siddiqua <siddiquaaimen@gmail.com>
2025-12-28 09:42:19 +0500

    Add boxes and tiles

--- SemiFinalGame/GameForm.Designer.cs
+++ SemiFinalGame/GameForm.Designer.cs
@@ -29,20 +29,79 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.platform_tilesblue;
+            pictureBox1.Location = new Point(-1, 544);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(677, 69);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.platform_tilesblue;
+            pictureBox2.Location = new Point(-1, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(677, 67);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.box;
+            pictureBox3.Location = new Point(271, 64);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(77, 157);
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.box;
+            pictureBox4.Location = new Point(426, 395);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(76, 154);
+            pictureBox4.TabIndex = 4;
+            pictureBox4.TabStop = false;
             // 
             // GameForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(643, 546);
+            ClientSize = new Size(650, 603);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
             Name = "GameForm";
-            Text = "Form1";
+            Text = "Game Form";
             Load += GameForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
     }
 }
